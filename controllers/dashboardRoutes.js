031c6f772e343700d5f0d83fb54afa62ffbbee51
Brandon <brandonkly@ucla.edu>
2023-02-04 02:17:51 -0800

    added get all for dashboard and homepage

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -10,18 +10,34 @@ const withAuth = require("../utils/auth");
 // TODO - create logic for the GET route for / that renders the dashboard homepage
 // It should display all of the posts created by the logged in user
 router.get("/", withAuth, async (req, res) => {
-  // TODO - retrieve all posts from the database for the logged in user
-  // render the dashboard template with the posts retrieved from the database
-  //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
-  res.render("admin-all-posts", { layout: "dashboard" });
-  // refer to admin-all-posts.handlebars write the code to display the posts
+
+  const postData = await Post.findAll({
+      where: { userId: req.session.userId },
+      include: [
+          {
+              model: {User},
+              attributes: ["username"],
+          },
+      ],
+      order: [["createdAt", "DESC"]],
+  });
+
+  const posts = postData.map(post => post.get({ plain: true }));
+
+  res.render("admin-all-posts", { layout: "dashboard", posts });
 });
 
 // TODO - create logic for the GET route for /new that renders the new post page
 // It should display a form for creating a new post
+router.get("/new", (req, res) => {
+
+});
 
 // TODO - create logic for the GET route for /edit/:id that renders the edit post page
 // It should display a form for editing an existing post
+router.get("/edit/:id", (req, res) => {
+
+});
 
 module.exports = router;
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,3 @@
-
 // homeroutes contains all the view routes that do not require any authentication
 const router = require("express").Router();
 const { Post, Comment, User } = require("../models/");
@@ -6,16 +5,20 @@ const { Post, Comment, User } = require("../models/");
 // TODO - work on GET route for getting all posts
 // this page can be viewed without logging in
 router.get("/", async (req, res) => {
-  // TODO - retrieve all posts from the database
-  // render the homepage template with the posts retrieved from the database
-  // refer to homepage.handlebars write the code to display the posts
-  res.render("homepage");
+  try {
+    const postData = await Post.findAll({order: [["createdAt", "DESC"]],});
+
+    const allPosts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", { allPosts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // TODO - create a GET route for getting a single post with its id
 // this page can be viewed without logging in
-
-
+router.get("/post/:id", (req, res) => {});
 
 // This route renders the login page, which has been completed for you
 router.get("/login", (req, res) => {
@@ -27,8 +30,6 @@ router.get("/login", (req, res) => {
   //render the login view otherwise, refer to login.handlebars
   res.render("login");
 });
-
-
 
 // This route renders the signup page, which has been completed for you
 router.get("/signup", (req, res) => {
@@ -42,4 +43,3 @@ router.get("/signup", (req, res) => {
 });
 
 module.exports = router;
-
