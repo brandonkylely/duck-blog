efe7ddf7cea23c42efe86d2ac21c021cb8079c10
Brandon <brandonkly@ucla.edu>
2023-03-15 05:23:25 -0700

    delete routes to dash now

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -13,7 +13,7 @@ const deletePost = async function (event) {
     });
   
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/dashboard");
     } else {
       alert("Failed to delete post");
     }
